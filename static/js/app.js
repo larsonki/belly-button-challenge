0c123d2103f7f94111b9af9458f359e75721b7d6
larsonki <kirsten.ifill.larson@gmail.com>
2022-09-28 10:47:44 -0500

    comments added to code

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,22 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
+// Initialize function that pulls the subject ID numbers and adds them to the drop-down to feed into building the charts.
 function init() {
     var dropDown = d3.select("#selDataset");
-
     // Fetch the JSON data
     d3.json(url).then(function (data) {
-        var subId = data.names;
-        subId.forEach((sample) => {
+        var sampleId = data.names;
+        sampleId.forEach((sample) => {
             dropDown.append("option").text(sample).property("value", sample)
         });
-        var initSample = subId[0];
-        buildMeta(initSample);
+        var initSample = sampleId[0];
+        buildDemo(initSample);
         buildCharts(initSample);
         // console.log(data);
     });
 };
 
+// Build charts function using the sample from the initialize function to create each of the charts.
 function buildCharts(sample) {
     d3.json(url).then(function (data) {
         let trace1 = {
@@ -24,10 +25,14 @@ function buildCharts(sample) {
     });
 };
 
-function buildMeta(sample) {
+// Build the demographic box again using the sample from the initialize function to create the demographic box that includes
+// the subject's information
+function buildDemo(sample) {
     var demo = d3.select("#sample-metadata");
     d3.json(url).then(function (data) {
         var metaData = data.metadata;
+        // Code adapted during study group with Doug
+        // Filter code adapted from https://observablehq.com/@observablehq/learn-javascript-introduction?collection=@observablehq/tutorial
         var metaDataSample = metaData.filter(row => row.id == sample);
         demo.selectAll("p").remove();
         metaDataSample.forEach((row) => {
@@ -36,40 +41,14 @@ function buildMeta(sample) {
             };
         });
     });
-
 };
 
+// Option changed function pulled from HTML file and used to update build charts and build demo functions when the value in the drop-down
+// changes
 function optionChanged(sample) {
-    buildMeta(sample);
+    buildDemo(sample);
     buildCharts(sample);
 };
 
+// Calling the initialize function to run.
 init();
-
-
-
-
-
-
-
-
-// d3.select("select").on("change", function(d) {
-//     var selected = d3.select("#selDataset").node().value;
-//     console.log(selected);
-// })
-
-// function optionChanged(val) {
-
-// };
-
-// function push(data) {
-
-// };
-
-// console.log(sampleValues);
-// console.log(otuIds);
-// console.log(otuLabels);
-
-// let trace1 = {
-//     x: 
-// }
