69d849eb1130b79d009fa38e7e13ed13ab5bb16a
larsonki <kirsten.ifill.larson@gmail.com>
2022-09-28 17:41:27 -0500

    code for gauge

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
 // Initialize function that pulls the subject ID numbers and adds them to the drop-down to feed into building the charts.
+// Function codes built up from tutoring session with Kelli Kennedy
 function init() {
     var dropDown = d3.select("#selDataset");
     // Fetch the JSON data
@@ -28,20 +29,16 @@ function buildCharts(sample) {
         var otuIdsSlice = otuIds.slice(0,10).reverse();
         var otuLabels = sampleInfo[0].otu_labels;
         var otuLabelsSlice = otuLabels.slice(0,10).reverse();
-        // var ylabel = []
+        var metaData = data.metadata;
+        var metaDataSample = metaData.filter(row => row.id == sample);
+        var wash = metaDataSample[0].wfreq;
 
-        // for (var i = 0; i < 10; i++) {
-        //     ylabel.append(`OTU ${otuIdsSlice[i]}`);
-        // }
-
-        console.log(sampleValues);
+        // console.log(metaData);
 
         // Create the first chart (bar chart)
         var trace1 = {
             x: sampleValuesSlice,
-            y: [`OTU ${otuIdsSlice[0]}`, `OTU ${otuIdsSlice[1]}`, `OTU ${otuIdsSlice[2]}`, `OTU ${otuIdsSlice[3]}`, `OTU ${otuIdsSlice[4]}`, 
-            `OTU ${otuIdsSlice[5]}`, `OTU ${otuIdsSlice[6]}`, `OTU ${otuIdsSlice[7]}`, `OTU ${otuIdsSlice[8]}`, `OTU ${otuIdsSlice[9]}`],
-
+            y: otuIdsSlice.map(item => `OTU ${item}`),
             type: "bar",
             orientation: "h",
             text: otuLabelsSlice,
@@ -58,7 +55,8 @@ function buildCharts(sample) {
                 size: sampleValues,
                 color: otuIds,
                 colorscale: "Earth"
-            }
+            },
+            text: otuIds
         };
         var data2 = [trace2];
         var layout = {
@@ -66,6 +64,40 @@ function buildCharts(sample) {
         };
 
         Plotly.newPlot("bubble", data2, layout);
+
+        // Create the gauge chart.
+        var data3 = [
+            {
+              domain: { x: [0, 1], y: [0, 1] },
+              value: wash,
+              title: {text: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week" },
+              type: "indicator",
+              mode: "gauge+number",
+              gauge: {
+                axis: { range: [null, 9] },
+                bar: { color: "black" },
+                steps: [
+                  { range: [0, 1], color: "rgba(114, 70, 28, 0.9)" },
+                  { range: [1, 2], color: "rgba(169, 146, 48, 0.9)" },
+                  { range: [2, 3], color: "rgba(199, 188, 58, 0.9)" },
+                  { range: [3, 4], color: "rgba(221, 226, 68, 0.9)" },
+                  { range: [4, 5], color: "rgba(66, 206, 58, 0.9)" },
+                  { range: [5, 6], color: "rgba(29, 184, 122, 0.9)" },
+                  { range: [6, 7], color: "rgba(15, 155, 165, 0.9)" },
+                  { range: [7, 8], color: "rgba(11, 117, 154, 0.9)" },
+                  { range: [8, 9], color: "rgba(0, 34, 125, 0.9)" },
+                ],
+                threshold: {
+                  line: { color: "black", width: 4 },
+                  thickness: 0.75,
+                  value: wash
+                }
+              }
+            }
+          ];
+          
+          var layout2 = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+          Plotly.newPlot('gauge', data3, layout2);
     });
 };
 
